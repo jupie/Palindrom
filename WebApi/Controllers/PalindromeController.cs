0077cb398f121b7196e0bedd0aa64c044e0a2377
jannik <jannik@upmann.net>
2025-04-24 18:32:05 +0200

    implement Controller

---
+++ WebApi/Controllers/PalindromeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Palindrom;
+using WebApi.UseCaseInteractors;
+
+namespace WebApi.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class PalindromeController
+{
+    private readonly IPalindromeInteractor _interactor;
+
+    public PalindromeController(IPalindromeInteractor interactor)
+    {
+        _interactor = interactor;
+    }
+
+    [HttpGet(Name = "BerechnePalindrom")]
+    public Palindromergebnis GetPalindromeFor(int eingabe)
+    {
+        return _interactor.BerechnePalindrome(eingabe);
+    }
+    [HttpPost(Name = "SpeicherPalindrom")]
+    public async Task PostPalindrome(int eingabe)
+    {
+        await _interactor.SpeicherPalindrome(eingabe);
+    }
+    
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Palindrom;
+using WebApi.Persistence;
+using WebApi.UseCaseInteractors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,12 +10,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddScoped<ITransformieren, Transformieren>(); 
+builder.Services.AddScoped<IPalindromeInteractor, PalindromeInteractor>(); 
+builder.Services.AddScoped<IPalindromeRepository, PalindromeRepository>();
+builder.Services.AddDbContext<PalindromeContext>(optionsBuilder => optionsBuilder.UseInMemoryDatabase("aDatabaseName")); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+   
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meine API v1");
+        c.RoutePrefix = string.Empty; // Swagger unter root anzeigen
+    });
     app.MapOpenApi();
 }
 
